c65ab00f85a515d5a94448983bd85422add66a87
lclem <clementelorenzo@gmail.com>
2023-11-28 11:44:05 +0100

    Batch second commit #127

---
+++ runner/logs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker logs librarian-runner
---
+++ runner/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+TOKEN="g i t h u b _ p a t _ 1 1 A B C B U P Q 0 U c 1 y d 5 g K 9 i 8 q _ n 5 N 3 Y C f g K A a M u v 0 X 1 z 5 T L 7 l E 1 8 R p A U P P R f t g U fs N q K U Q V U B J 7 O B p T BF6 r3 R "
+TOKEN=$(echo $TOKEN | tr -d " ")
+GITHUB_ACCESS_TOKEN=$TOKEN ./start lclem librarian
