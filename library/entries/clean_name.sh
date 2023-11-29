73267a35cb0abfa16957eca3c9c5b3c5097afa69
lclem <clementelorenzo@gmail.com>
2023-11-29 22:46:47 +0100

    up

--- library/entries/clean_name.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NAME="${1}"
-NEW_NAME="${NAME//:/_}"
-
-echo "MV ${NAME} => ${NEW_NAME}"
-git mv ${NAME} ${NEW_NAME}
-echo DONE
--- library/entries/clean_names.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find . -name \*:\* -exec ./clean_name.sh {} \;
