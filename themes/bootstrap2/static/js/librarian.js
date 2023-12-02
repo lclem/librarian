8d5726424411dfce9d686e2ef484f45d79d55544
Lorenzo C <clementelorenzo@gmail.com>
2023-12-02 09:15:43 +0100

    Update librarian.js

--- themes/bootstrap2/static/js/librarian.js
+++ themes/bootstrap2/static/js/librarian.js
@@ -9,6 +9,18 @@ let bibStr = "";
 
 var bibFile;
 
+async function storkInit(siteurl) {
+		stork.initialize(siteurl + "/theme/js/stork-1.6/stork.wasm");
+		options = {showScores: false};
+		
+		const deferred = async function() {
+		stork.register("sitesearch", siteurl + "/search-index.st", options);
+		console.log("stork initialised");
+		};
+		
+		deferred();
+}
+
 stork_input.addEventListener("change", updateSearch, false);
 stork_input.addEventListener("paste", detectPaste, false);
 
