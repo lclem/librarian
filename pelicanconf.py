c85303157abb0b1f5f9b0532f258c3288cc80f90
Lorenzo C <clementelorenzo@gmail.com>
2023-12-05 09:59:15 +0100

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -87,7 +87,7 @@ PAGE_SAVE_AS = 'pages/{slug}/index.html'
 
 #List of templates that are used directly to render content. Typically direct templates are used to generate index pages for collections of content (e.g., category and tag index pages). If the author, category and tag collections are not needed, set DIRECT_TEMPLATES = ['index', 'archives']
 DIRECT_TEMPLATES = ['author', 'archives', 'authors_bad', 'nopdf', 'home', 'nocover'] # 'index', 
-PAGINATED_TEMPLATES = {'home': 12, 'nopdf': None} # 'index' : 20, 
+PAGINATED_TEMPLATES = {'index': 12, 'nopdf': None} # 'index' : 20, 
 #A mapping containing template pages that will be rendered with the blog entries.
 #If you want to generate custom pages besides your blog entries, you can point any Jinja2 template file with a path pointing to the file and the destination path for the generated file.
 #For instance, if you have a blog with three static pages — a list of books, your resume, and a contact page — you could have:
